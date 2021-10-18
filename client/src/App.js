18dad3f6e58ac40ccfbc818d6bcc8ff0136eaea8
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-10-18 17:02:44 -0600

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,7 @@
-import { useState } from 'react';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  const handleClick = (e) => {
-    {e.target.name === 'increase' ? setCount(count + 1) : setCount(count - 1)}
-  }
-  
+function App() {  
   return (
     <div className="App">
-      <h1>Current Count: {count}</h1>
-      <button name='increase' onClick={handleClick}>Increase Count</button>
-      <button name='decrease' onClick={handleClick}>Decrease Count</button>
+      My App
     </div>
   );
 }
